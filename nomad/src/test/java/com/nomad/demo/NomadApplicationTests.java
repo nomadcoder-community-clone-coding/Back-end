49f08897088feb9ef58b2320bccd6ed39735c420
ejhee1 <71870316+ejhee1@users.noreply.github.com>
2021-05-30 17:21:01 +0900

    DB_Spring

---
+++ nomad/src/test/java/com/nomad/demo/NomadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomad.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
