39a4b741cc11760db963555efb4b8322038838c8
ejhee1 <71870316+ejhee1@users.noreply.github.com>
2021-05-30 17:16:24 +0900

    mysql_spring

---
+++ nomad/backEnd/nomad/src/main/java/com/nomad/demo/NomadApplication.java
@@ -0,0 +1,13 @@
+package com.nomad.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomadApplication.class, args);
+	}
+
+}
