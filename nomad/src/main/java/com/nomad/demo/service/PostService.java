3f3c6dcb8e28213cec6e81f9177c665af011d2ed
hoit1302 <jueun000101@gmail.com>
2021-05-31 18:51:39 +0900

    feat(PostService): add get one post logic

---
+++ nomad/src/main/java/com/nomad/demo/service/PostService.java
@@ -0,0 +1,38 @@
+package com.nomad.demo.service;
+
+import com.nomad.demo.domain.category.Category;
+import com.nomad.demo.domain.post.DetailResponseDto;
+import com.nomad.demo.domain.post.Post;
+import com.nomad.demo.repository.CategoryRepository;
+import com.nomad.demo.repository.PostRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class PostService {
+	private final PostRepository postRepository;
+	private final CategoryRepository categoryRepository;
+
+	public Optional<Post> getPost(Long id){
+		return postRepository.findById(id);
+	}
+	
+	@Transactional
+	public DetailResponseDto getPostDetail(Post post) {
+		return post.toDetailResponseDto(getCategory(post));
+	}
+	
+	public Category getCategory(Post post) {
+//		Optional<Category> category = categoryRepository.findById(post.getCategoryId());
+//		if (category.isPresent())
+//			return category.get();
+
+
+			return categoryRepository.findById(post.getCategoryId()).get();
+
+	}
+}
